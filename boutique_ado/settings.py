f7470ac374eb720d1a0bd2de58317bb5792a25d9
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-10-07 14:34:22 +0000

    prepare images for deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,11 +180,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-code-along '
     AWS_S3_REGION_NAME = 'eu-north-1'
